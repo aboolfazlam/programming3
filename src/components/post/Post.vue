c6c70b723aa3c17e348a5e6283966c4f4b11950a
aboolfazlam7 <abolfazlhadad7@gmail.com>
2025-07-07 20:17:13 +0330

    test

---
+++ src/components/post/Post.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { defineProps } from 'vue';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+const props = defineProps({
+   posts:Object
+})
+</script>
+<template>
+  <table class="table table-striped mt-4">
+    <thead>
+      <tr>
+        <th scope="col">userId</th>
+        <th scope="col">title</th>
+        <th scope="col">body</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(post,index) in posts" :key="post.id">
+        <td scope="row">{{index + 1}}</td>
+        <td v-text="post.title"></td>
+        <td v-text="post.body"></td>
+        <td>
+            <a href="#" class="p-2">
+                <i class="bi bi-eye-fill"></i>
+            </a>
+            <a href="#" class="text-success p-2">
+                <i class="bi bi-pencil-fill"></i>
+            </a>
+            <a href="#" class="text-danger p-2">
+                <i class="bi bi-trash-fill"></i>
+            </a>
+        </td>
+      </tr>
+      
+    </tbody>
+  </table>
+</template>
+<style scoped>
+thead th{
+    background-color: black;
+    color: white;
+}
+</style>
---
+++ src/components/user/ShowUser.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { onMounted, ref, watch } from "vue";
+import { useRoute } from "vue-router";
+import axios from "axios";
+import IndexUser from "./IndexUser.vue";
+let user = ref([]);
+const route = useRoute();
+function get_user(id) {
+
+  axios
+    .get(`https://jsonplaceholder.typicode.com/users/${id}`)
+    .then(function (response) {
+      // handle success
+      user.value = response.data;
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+}
+onMounted (() => {
+  get_user(route.params.id)
+})
+watch(()=>route.params.id,(new_id)=>{
+  get_user(new_id)
+})
+</script>
+<template>
+  <div class="container">
+
+     <div class="col-sm-6 mb-3 mb-sm-0">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">{{ user.name}}</h5>
+        <p class="card-text">{{user.username}}</p>
+        <p class="card-text">{{user.phone}}</p>
+        <p class="card-text">{{user.email}}</p>
+        <router-link :to="{name:IndexUser}"> </router-link>
+  
+      </div>
+    </div>
+  </div>
+
+
+  </div>
+</template>
